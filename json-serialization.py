7c3072967fd49efdb2a5aac59981b400801dc255
sharsampcisco <78094773+sharsampcisco@users.noreply.github.com>
2021-01-27 21:21:31 +0530

    Add files via upload

---
+++ json-serialization.py
@@ -0,0 +1,18 @@
+import json
+
+UserA = {
+    "Name" : "Vandana",
+    "Age" : 26,
+    "Skills" : ['python','c++','networking'],
+    "Location" : "Bangalore"
+}
+jsonstring = json.dumps(UserA)
+print("First user's detail is : "+jsonstring)
+#print(jsonstring['Name']) 
+#print(type(jsonstring))
+
+"""
+Uncomment below to Deserialize 
+"""
+jsonobj = json.loads(jsonstring)
+print(jsonobj['Name']+"'s Age is : ", jsonobj['Age'])
