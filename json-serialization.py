f4b058c64c35e02c4ce4066bf465cdeec65cb354
sharsampcisco <78094773+sharsampcisco@users.noreply.github.com>
2021-01-27 21:25:33 +0530

    Update json-serialization.py

--- json-serialization.py
+++ json-serialization.py
@@ -8,11 +8,13 @@ UserA = {
 }
 jsonstring = json.dumps(UserA)
 print("First user's detail is : "+jsonstring)
+
+"""
+To make it fail uncomment the below
+"""
 #print(jsonstring['Name']) 
 #print(type(jsonstring))
 
-"""
 Uncomment below to Deserialize 
 jsonobj = json.loads(jsonstring)
 print(jsonobj['Name']+"'s Age is : ", jsonobj['Age'])
-"""
